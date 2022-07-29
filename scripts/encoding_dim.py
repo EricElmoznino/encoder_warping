623a427036be5503a06eb51aab6148b8156d85ec
EricElmoznino <eric.elmoznino@gmail.com>
2022-07-28 22:21:23 -0400

    Changed a default run name

--- scripts/encoding_dim.py
+++ scripts/encoding_dim.py
@@ -18,7 +18,8 @@ def main(cfg: DictConfig) -> None:
     # Create save directory
     if cfg.run_name is None:
         cfg.run_name = "_".join(
-            [f"dataset={cfg.data.name}"] + [f"{k}={v}" for k, v in cfg.model.items()]
+            [f"neural-data={cfg.data.name}"]
+            + [f"{k}={v}" for k, v in cfg.model.items()]
         )
     save_dir = f"saved_runs/encoding_dim/{cfg.run_name}"
     shutil.rmtree(save_dir, ignore_errors=True)
