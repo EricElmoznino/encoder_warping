be8ddc6a91aee18cb2079251b93a117b93a4ce8d
swadhwa5 <swadhwa5@login03.rockfish.cluster>
2022-09-28 16:36:27 -0400

    added plot for renet18 pretrained vs not pretrained

---
+++ simulations/dim_complexity/dim_R2/plot.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def main():
+    # Read data
+    df1 = pd.read_csv ('/home/swadhwa5/projects/encoder_warping/saved_runs/encoding_dim/neural-data=nsd_arch=resnet18_dataset=imagenet_task=object-classification_layer=avgpool/results.csv')
+    df2 = pd.read_csv ('/home/swadhwa5/projects/encoder_warping/saved_runs/encoding_dim/neural-data=nsd_arch=resnet18_dataset=None_task=object-classification_layer=avgpool/results.csv')
+    # gca stands for 'get current axis'
+    ax = plt.gca()
+
+    df1.plot(kind='line',x='low_dim',y='test_r2',ax=ax, label='Res18Pre')
+    df2.plot(kind='line',x='low_dim',y='test_r2', color='red', ax=ax, label='Res18NoPre')
+    plt.xlabel('Number of dimensions')
+    plt.ylabel('R^2')
+
+    plt.savefig('/home/swadhwa5/projects/encoder_warping/simulations/dim_complexity/saved_figures/dim_R2_plot.png')
+
+
+if __name__ == "__main__":
+    main()
