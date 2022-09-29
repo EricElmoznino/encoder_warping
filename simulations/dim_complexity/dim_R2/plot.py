ad3613a61fffe6f49a46d3db671413d891a5ac8c
swadhwa5 <swadhwa5@jh.edu>
2022-09-29 11:18:24 -0400

    added plots for layer3

--- simulations/dim_complexity/dim_R2/plot.py
+++ simulations/dim_complexity/dim_R2/plot.py
@@ -2,6 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 def main():
+    # avgpool
     # Read data
     df1 = pd.read_csv ('/home/swadhwa5/projects/encoder_warping/saved_runs/encoding_dim/neural-data=nsd_arch=resnet18_dataset=imagenet_task=object-classification_layer=avgpool/results.csv')
     df2 = pd.read_csv ('/home/swadhwa5/projects/encoder_warping/saved_runs/encoding_dim/neural-data=nsd_arch=resnet18_dataset=None_task=object-classification_layer=avgpool/results.csv')
@@ -12,8 +13,23 @@ def main():
     df2.plot(kind='line',x='low_dim',y='test_r2', color='red', ax=ax, label='Res18NoPre')
     plt.xlabel('Number of dimensions')
     plt.ylabel('R^2')
+    plt.title('Resnet18 avgpool')
+    plt.savefig('/home/swadhwa5/projects/encoder_warping/simulations/dim_complexity/saved_figures/dim_R2_plot_avgpool.png')
 
-    plt.savefig('/home/swadhwa5/projects/encoder_warping/simulations/dim_complexity/saved_figures/dim_R2_plot.png')
+    # layer 3
+    # Read data
+    plt.clf()
+    df3 = pd.read_csv ('/home/swadhwa5/projects/encoder_warping/saved_runs/encoding_dim/neural-data=nsd_arch=resnet18_dataset=imagenet_task=object-classification_layer=layer3/results.csv')
+    df4 = pd.read_csv ('/home/swadhwa5/projects/encoder_warping/saved_runs/encoding_dim/neural-data=nsd_arch=resnet18_dataset=None_task=object-classification_layer=layer3/results.csv')
+    # gca stands for 'get current axis'
+    ax = plt.gca()
+
+    df3.plot(kind='line',x='low_dim',y='test_r2',ax=ax, label='Res18Pre')
+    df4.plot(kind='line',x='low_dim',y='test_r2', color='red', ax=ax, label='Res18NoPre')
+    plt.xlabel('Number of dimensions')
+    plt.ylabel('R^2')
+    plt.title('Resnet18 layer3')
+    plt.savefig('/home/swadhwa5/projects/encoder_warping/simulations/dim_complexity/saved_figures/dim_R2_plot_layer3.png')
 
 
 if __name__ == "__main__":
