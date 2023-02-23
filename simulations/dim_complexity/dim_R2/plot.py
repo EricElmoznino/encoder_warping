89022507ec91bb2ac1502699518db8a3d177bf74
swadhwa5 <swadhwa5@jh.edu>
2023-02-23 12:44:16 -0500

    added plots for avgpool and layer1 for nsd

--- simulations/dim_complexity/dim_R2/plot.py
+++ simulations/dim_complexity/dim_R2/plot.py
@@ -6,13 +6,8 @@ def main():
     # layer 3
     # Read data
     plt.clf()
-<<<<<<< HEAD
     df3 = pd.read_csv ('/home/swadhwa5/projects/encoder_warping/saved_runs/encoding_dim/neural-data=nsd_arch=resnet18_dataset=imagenet_task=object-classification_layer=avgpool/results.csv')
     df4 = pd.read_csv ('/home/swadhwa5/projects/encoder_warping/saved_runs/encoding_dim/neural-data=nsd_arch=resnet18_dataset=None_task=object-classification_layer=avgpool/results.csv')
-=======
-    df3 = pd.read_csv ('/home/swadhwa5/projects/encoder_warping/saved_runs/encoding_dim/neural-data=nsd_arch=resnet18_dataset=imagenet_task=object-classification_layer=layer4/results.csv')
-    df4 = pd.read_csv ('/home/swadhwa5/projects/encoder_warping/saved_runs/encoding_dim/neural-data=nsd_arch=resnet18_dataset=None_task=object-classification_layer=layer4/results.csv')
->>>>>>> parent of e90e11a... added plots for majaj and nsd
     # gca stands for 'get current axis'
     df3['low_dim'] = np.log10(df3['low_dim'])
     df4['low_dim'] = np.log10(df4['low_dim'])
@@ -22,14 +17,8 @@ def main():
     df4.plot(kind='line',x='low_dim',y='test_r2', color='red', ax=ax, label='Res18NoPre')
     plt.xlabel('Number of dimensions')
     plt.ylabel('R^2')
-<<<<<<< HEAD
     plt.title('Resnet18 avgpool Nsd')
-    plt.savefig('/home/swadhwa5/projects/encoder_warping/simulations/dim_complexity/saved_figures/nsd/dim_R2_plot_avgpool.png')
-=======
-    plt.title('Resnet18 layer3')
-    plt.savefig('/home/swadhwa5/projects/encoder_warping/simulations/dim_complexity/saved_figures/dim_R2_plot_layer4.png')
-
->>>>>>> parent of e90e11a... added plots for majaj and nsd
+    plt.savefig('/home/swadhwa5/projects/encoder_warping/simulations/dim_complexity/saved_figures/test/dim_R2_plot_avgpool.png')
 
 if __name__ == "__main__":
     main()
